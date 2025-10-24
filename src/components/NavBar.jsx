8ee651dc72168878578752db94613088f904ddf5
alexistarantos <alexistarantos@gmail.com>
2025-10-24 21:03:50 +0300

    - fix logo click

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,10 +14,10 @@ function NavBar() {
         <nav className="navbar">
             <div className="navbar-container">
                 <div className="navbar-logo">
-                    <Link to="/" className="navbar-logo-link" onClick={() => window.location.href = '/'}>
+                    <div className="navbar-logo-link" onClick={() => window.location.href = '/'}>
                         <img src={logo} alt="MovieFlix Logo" width={28} height={28} />
                         MOVIEFLIX
-                    </Link>
+                    </div>
                 </div>
                 <div className="navbar-links">
                     <Link to="/" className="nav-link">Home</Link>
